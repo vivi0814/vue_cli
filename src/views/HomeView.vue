ca3c55d298eec4a7548d716e4c80b13d512fd296
vivi0814 <vivian91949@gmail.com>
2022-12-25 20:53:38 +0800

    add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <HelloWorld title="早安" msg="午安"/>
     <HelloWorld title="QQ" msg="1"/>
     <HelloWorld title="@@" msg="2"/>
